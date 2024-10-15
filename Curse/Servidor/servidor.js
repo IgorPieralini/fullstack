36cb4252fca6583ba8849d5e719f3438da0c4238
Igor Pieralini <igorpieralini@gmail.com>
2024-10-15 17:07:19 -0300

    highjghjhgj

--- Curse/Servidor/servidor.js
+++ Curse/Servidor/servidor.js
@@ -1,2 +1,10 @@
-require("colors");
-console.log('olá mundo'.rainbow)
+let http = require('http');
+var express = require('express');
+
+var app = express();
+app.use(express.static('./public'));
+
+var server = http.createServer(app);
+server.listen(3000)
+
+console.log('Servidor rodando')
